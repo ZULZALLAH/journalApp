4df333466be3a13241eb33d4d78933ce42476ff1
ZULZALLAH <saifahmadkhan02@gmail.com>
2024-11-18 00:30:35 +0530

    Add commenting all test

--- src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
@@ -33,28 +33,28 @@ public class UserDetailsServiceImplTests {
     // So, we used @InjectMocks,@Mock,@BeforeALL,etc
 
     //@Autowired
-    @InjectMocks // it take all @Mock field which using this class to inject in it bean class. without using spring context outside it.
-    private UserDetailsServiceImpl userDetailsService;
+  //  @InjectMocks // it take all @Mock field which using this class to inject in it bean class. without using spring context outside it.
+  //  private UserDetailsServiceImpl userDetailsService;
 
     //Spring Application context take all details of user to check from DB ,if not conect to db then pass dummy data.
     //Mockito used for test method but we does not interact with DB we can pass dummy data to test that method.
    // @Mock using this annotation when run this test application class So, it takes data from DB without using dummy data we intialize thenReturn(User.builder().username("KK").password("ASDFSW") to use for test.
     //So here using @Mock does not disable bean of userRepository when we run we get all details of user related to it.
    // @MockBean // this annotation will diable the bean of userRepository to not fetch data from DB ,take dummy data to test method run again this class as debug
-    @Mock // using again we take this and inject this in UserDetailsServiceImpl where i used @InjectMocks bean in it & also remove @SpringBootTest
+   // @Mock // using again we take this and inject this in UserDetailsServiceImpl where i used @InjectMocks bean in it & also remove @SpringBootTest
     // but then also userRepository are null then we used @BeforeEach ,we get NullPointerException to overcome we used (MockitoAnnotations.initMocks() using only class we used this keyword ,There no used of Spring Context we are outside of Spring context
-    private UserRepository userRepository;
+  //  private UserRepository userRepository;
 
-    @BeforeEach
-    void setUp(){
-        MockitoAnnotations.openMocks(this);
-    }
+//    @BeforeEach
+//    void setUp(){
+//        MockitoAnnotations.openMocks(this);
+//    }
 
-    @Test
-    void loadUserByUsernameTests(){
-        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("KK").password("ASDFSW").roles(new ArrayList<>()).build());
-       // when(userRepository.findByUsername("Saif")).thenReturn(User.builder().username("Saif").password("ASDFSW").build());
-        UserDetails user = userDetailsService.loadUserByUsername("KK");
-        Assertions.assertNotNull(user);
-    }
+//    @Test
+//    void loadUserByUsernameTests(){
+//        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("KK").password("ASDFSW").roles(new ArrayList<>()).build());
+//       // when(userRepository.findByUsername("Saif")).thenReturn(User.builder().username("Saif").password("ASDFSW").build());
+//        UserDetails user = userDetailsService.loadUserByUsername("KK");
+//        Assertions.assertNotNull(user);
+//    }
 }
--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -12,59 +12,61 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserServiceTests {
+
     // when we run application without using @SpringBootTest  i got NullPointer Exception because userRepository,userService,journalService,etc are bean null .So,Spring application context are null that why getting Exception. To overcome we use this annotation @SpringbootTest to insert bean in this class or interface while performing test.
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserService userService;
 
-    @Autowired
-    private UserService userService;
+//    @Test
+//    public void  testAdd(){
+//        assertEquals(4,2+2);
+//    }
 
-    @Test
-    public void  testAdd(){
-        assertEquals(4,2+2);
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @ArgumentsSource(UserArgumentProvider.class)
-    public void createUserTest(User user){
-        System.out.println("Condition value: " + user.getUsername());
-        assertTrue(userService.createUserTest(user));
-    }
+//    @Disabled
+//    @ParameterizedTest
+//    @ArgumentsSource(UserArgumentProvider.class)
+//    public void createUserTest(User user){
+//        System.out.println("Condition value: " + user.getUsername());
+//        assertTrue(userService.createUserTest(user));
+//    }
 
     // @Disabled // this annotation describe that if we not test this method then disabled it
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "JB",
-            "NL",
-            "KK"
-    })
+//    @ParameterizedTest
+//    @ValueSource(strings = {
+//            "JB",
+//            "NL",
+//            "KK"
+//    })
 //    @EnumSource(names = {
 //            "JB",
 //            "NL",
 //            "KK"
 //    })
-   // @Test
-    public void  testFindByUserName(String name){
-        User user = userRepository.findByUsername(name);
+    // @Test
+//    public void  testFindByUserName(String name){
+//        User user = userRepository.findByUsername(name);
 //        assertNotNull(userRepository.findByUsername(name),"failed for : " + name);
 //        assertTrue(!user.getJournalEntry().isEmpty());
-    }
-    @Disabled
-    @ParameterizedTest
-    @CsvSource({
-            "1,1,2",
-            "2,10,12",
-            "3,3,6"
-    })
-    public void test(int a, int b, int expected){
-        assertEquals(expected, a + b);
-    }
+    //  }
+//    @Disabled
+//    @ParameterizedTest
+//    @CsvSource({
+//            "1,1,2",
+//            "2,10,12",
+//            "3,3,6"
+//    })
+//    public void test(int a, int b, int expected){
+//        assertEquals(expected, a + b);
+//    }
 
 
 }
